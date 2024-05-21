cc7f1ef70b60c6decd030ec27ca0d85455482a17
morshidulrahman <boydanger416@gmail.com>
2024-05-22 00:04:48 +0600

    singup

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -5,7 +5,13 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { TbFidgetSpinner } from "react-icons/tb";
 const SignUp = () => {
-  const { createUser, updateUserProfile, loading, setLoading } = useAuth();
+  const {
+    createUser,
+    updateUserProfile,
+    loading,
+    setLoading,
+    signInWithGoogle,
+  } = useAuth();
 
   const navigate = useNavigate();
 
@@ -36,6 +42,17 @@ const SignUp = () => {
       toast.error(e.message);
     }
   };
+
+  const handleSingup = async () => {
+    try {
+      await signInWithGoogle();
+      toast.success("singup successful");
+      navigate("/");
+    } catch (e) {
+      toast.error(e.message);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
@@ -128,11 +145,15 @@ const SignUp = () => {
           </p>
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
-        <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
+        <button
+          onClick={handleSingup}
+          disabled={loading}
+          className="disabled:cursor-not-allowed flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+        >
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className="px-6 text-sm text-center text-gray-400">
           Already have an account?{" "}
           <Link
